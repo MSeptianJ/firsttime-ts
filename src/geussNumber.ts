bf859a56ddef4082b99c025d00ff5598e61a76ae
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-06-20 13:53:30 +0700

    Change Geussing Number to funciton

--- src/geussNumber.ts
+++ src/geussNumber.ts
@@ -1,21 +1,20 @@
 import userInput from './utils';
 
-// Random number from 1 - 10
-const numberToGuess: number = Math.floor(Math.random() * 10) + 1;
-// This variable is used to determine if the app should continue prompting the user for input
-let foundCorrectNumber: boolean = false;
+const GeussNumber = (): void => {
+	const numberToGuess: number = Math.floor(Math.random() * 10) + 1;
+	let foundCorrectNumber: boolean = false;
 
-while (!foundCorrectNumber) {
-	// Get user input
-	let guess: number = userInput('Guess a number from 1 to 10: ');
-	// Convert the string input to a number
-	guess = Number(guess);
+	while (!foundCorrectNumber) {
+		let guess: number = userInput('Guess a number from 1 to 10: ');
+		guess = Number(guess);
 
-	// Compare the guess to the secret answer and let the user know.
-	if (guess === numberToGuess) {
-		console.log('Congrats, you got it!');
-		foundCorrectNumber = true;
-	} else {
-		console.log('Sorry, guess again!');
+		if (guess === numberToGuess) {
+			console.log('Congrats, you got it!');
+			foundCorrectNumber = true;
+		} else {
+			console.log('Sorry, guess again!');
+		}
 	}
-}
+};
+
+export default GeussNumber;
